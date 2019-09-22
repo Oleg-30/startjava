b2cd7b8d734a42f74936b371cd2465a48b1726a7
Oleg-30 <germolebu@gmail.com>
2019-09-22 19:49:42 +0300

    Implement HW.1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main(String[] args) {
+	long proName = 2100;// 
+	byte numberOfCores = 2;
+	double proSpeed = 3.1d;
+	char size = 'B';
+	char giga = 'G';
+	char mega = 'M';
+	char herz1 = 'H';
+	char herz2 = 'z';
+	int numberOfThreads = 4;
+	short smartCache = 3;
+	float ram = 31.9f;
+	boolean t = true;
+	System.out.println("CPU Intel Core i3-" + proName + ", number of cores " + numberOfCores);
+	System.out.println("Processor speed " + proSpeed + giga + herz1 + herz2 + ", number of threads " + numberOfThreads);
+	System.out.println("Smart Cache " + smartCache + mega + size + ", RAM " + ram + mega + size);
+	System.out.println(t);
+	}
+}
